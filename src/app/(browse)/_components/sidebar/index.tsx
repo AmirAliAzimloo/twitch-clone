512cae1fc2b07b2ee52ee8f682beed1ff9a79214
Amir Ali <amiraliazimloo123@gmail.com>
2024-07-26 01:09:09 +0330

    feat: Add the recommended users logic for the Sidebar component.

--- src/app/(browse)/_components/sidebar/index.tsx
+++ src/app/(browse)/_components/sidebar/index.tsx
@@ -1,5 +1,5 @@
 // import { getFollowedUsers } from "@/lib/follow-service";
-// import { getRecommended } from "@/lib/recommended-service";
+import { getRecommended } from "@/lib/recommended-service";
 
 
 import { Wrapper } from "./wrapper";
@@ -8,24 +8,24 @@ import {
   Toggle, 
   ToggleSkeleton
 } from "./toggle";
-// import { 
-//   Recommended, 
-//   RecommendedSkeleton
-// } from "./recommended";
+import { 
+  Recommended, 
+  RecommendedSkeleton
+} from "./recommended";
 
 
 export const Sidebar = async () => {
-//   const recommended = await getRecommended();
+  const recommended = await getRecommended();
 //   const following = await getFollowedUsers();
 
   return (
     <>
      <Wrapper>
        <Toggle />
-       {/* <div className="space-y-4 pt-4 lg:pt-0">
-         <Following data={following} />
+       <div className="space-y-4 pt-4 lg:pt-0">
+         {/* <Following data={following} /> */}
          <Recommended data={recommended} />
-       </div> */}
+       </div>
      </Wrapper>
     </>
   );
@@ -35,8 +35,8 @@ export const SidebarSkeleton = () => {
   return (
     <aside className="fixed left-0 flex flex-col w-[70px] lg:w-60 h-full bg-background border-r border-[#2D2E35] z-50">
       <ToggleSkeleton />
-      {/* <FollowingSkeleton />
-      <RecommendedSkeleton /> */}
+      {/* <FollowingSkeleton /> */}
+      <RecommendedSkeleton />
     </aside>
   );
 };
