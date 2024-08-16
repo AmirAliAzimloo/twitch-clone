dc28884b996792c00ca8410b7aaccb3606e95b6f
Amir Ali <amiraliazimloo123@gmail.com>
2024-08-16 18:37:25 +0330

    Implement the home page.

--- src/app/(browse)/(home)/page.tsx
+++ src/app/(browse)/(home)/page.tsx
@@ -1,9 +1,13 @@
-export default function Home() {
+import { Suspense } from "react";
+
+import { Results, ResultsSkeleton } from "./_components/results";
+
+export default function Page() {
   return (
-    <>
-    <div>
-      Home Page
+    <div className="h-full p-8 max-w-screen-2xl mx-auto">
+      <Suspense fallback={<ResultsSkeleton />}>
+        <Results />
+      </Suspense>
     </div>
-    </>
   );
-}
+};
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhooks(.*)', '/', '/api/uploadthing']);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/webhooks(.*)', '/', '/api/uploadthing', '/:username', '/search']);
 
 export default clerkMiddleware((auth, request) => {
   if(!isPublicRoute(request)) {
