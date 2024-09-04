17f0f2be6732b209d0182228022d699be2342012
Amir Ali <amiraliazimloo123@gmail.com>
2024-09-05 01:06:09 +0330

    Refactor the community page.

--- src/app/(dashboard)/u/[username]/community/page.tsx
+++ src/app/(dashboard)/u/[username]/community/page.tsx
@@ -19,7 +19,6 @@ const CommunityPage = async () => {
   return ( 
     <div className="p-6">
       <div className="mb-4">
-        
         <h1 className="text-2xl font-bold">
           Community Settings
         </h1>
