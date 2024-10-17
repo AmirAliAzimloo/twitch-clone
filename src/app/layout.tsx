181b16005dbd04872a68624302e9e67f40080474
Amir Ali <amiraliazimloo123@gmail.com>
2024-10-17 19:37:10 +0330

    Refactor the RootLayout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
+    <ClerkProvider 
     appearance={{
       baseTheme: [neobrutalism]
     }}
