eaf8295d9de720c4c88783e7132e947f9568036d
Amir Ali <amiraliazimloo123@gmail.com>
2024-11-18 21:49:10 +0330

    Refactor the main layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode; 
 }>) {
   return (
     <ClerkProvider 
