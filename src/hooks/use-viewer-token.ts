6e067b590e85b8964b274ccba0ff4919cb21df41
Amir Ali <amiraliazimloo123@gmail.com>
2025-03-29 23:29:15 +0330

    Refactor formatting in useViewerToken hook for improved readability

--- src/hooks/use-viewer-token.ts
+++ src/hooks/use-viewer-token.ts
@@ -14,7 +14,9 @@ export const useViewerToken = (hostIdentity: string) => {
       try {
         const viewerToken = await createViewerToken(hostIdentity);
         setToken(viewerToken);
-        const decodedToken = jwtDecode(viewerToken) as JwtPayload & { name?: string }
+        const decodedToken = jwtDecode(viewerToken) as JwtPayload & {
+          name?: string;
+        };
         const name = decodedToken?.name;
         const identity = decodedToken.jti;
         if (identity) {
@@ -26,7 +28,7 @@ export const useViewerToken = (hostIdentity: string) => {
       } catch {
         toast.error("Something went wrong");
       }
-    }
+    };
 
     createToken();
   }, [hostIdentity]);
